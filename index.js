f4f824f92c01cdd24e76cf20cd79a76c881f34d5
Ivo <ivo.paunov@gmail.com>
2018-01-28 19:10:40 +0200

    fix index

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const AdSlot = require('./models/AdSlot')
 const AdUnit = require('./models/AdUnit')
 const Campaign = require('./models/Campaign')
 const Channel = require('./models/Account')
-const Item = require('./models/Account')
-const Base = require('./models/Account')
-const Bid = require('./models/Account')
+const Item = require('./models/Item')
+const Base = require('./models/Base')
+const Bid = require('./models/Bid')
 
 module.exports = {
     Account,

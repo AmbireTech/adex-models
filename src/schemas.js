44206f3eed2198fc81d938775bf368ac0f522fee
simzzz <simeon_nakov@hotmail.com>
2019-04-09 18:56:06 +0300

    Changed errors to text, removed err constatns, modified title/desc validation

--- src/schemas.js
+++ src/schemas.js
@@ -1,14 +1,5 @@
 const { Joi } = require('celebrate')
 const types = require('./constants').AdUnitsTypes
-<<<<<<< HEAD
-const ipfsRegex = /(ipfs):\/\/(.){46}?$/
-const mimeTypes = ['image/jpeg', 'image/png']
-const addressRegex = /^0x[0-9A-Fa-f]{40}$/
-const signatureRegex = /^0x[0-9A-Fa-f]{130}$/
-const hashRegex = /^0x[0-9A-Fa-f]{64}$/
-const typeRegex = /^iab_flex_.{1,}/
-const validModes = [0, 1, 2, 3, 4]
-=======
 const mimeTypes = require('./constants').MimeTypes
 const {
 	ipfsRegex,
@@ -18,7 +9,6 @@ const {
 	hashRegex
 } = require('./constants').Regexes
 const validModes = [0, 1, 2]
->>>>>>> d523306d575978a8acf13594c0ef299e899bec75
 const roles = ['advertiser', 'publisher']
 
 const TYPE_ERR_SLOT = new Error('Invalid adSlot type!')
@@ -53,67 +43,67 @@ const TD_VALUE_ERR = new Error('Invalid typed data value')
 
 module.exports = {
 	adSlotPost: {
-		type: Joi.string().regex(typeRegex).allow(types).required().error(TYPE_ERR_SLOT),
+		type: Joi.string().regex(typeRegex).allow(types).required().error('TYPE_ERR_SLOT'),
 		tags: Joi.array().items({
-			tag: Joi.string().required().error(TAG_NAME_ERR),
-			score: Joi.number().min(0).max(100).required().error(TAG_SCORE_ERR)
+			tag: Joi.string().required().error('TAG_NAME_ERR'),
+			score: Joi.number().min(0).max(100).required().error('TAG_SCORE_ERR')
 		}).required(),
-		created: Joi.date().timestamp().required().error(CREATED_DATE_ERR_SLOT),
-		title: Joi.string().max(120).required().error(TITLE_ERR_SLOT),
-		description: Joi.string().max(300).required().error(DESC_ERR_SLOT),
-		fallbackMediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error(IPFS_URL_ERR),
-		fallbackMediaMime: Joi.string().valid(mimeTypes).required().error(MEDIA_MIME_ERR),
-		fallbackTargetUrl: Joi.string().uri().required().error(TARGET_URL_ERR),
-		archived: Joi.bool().optional().error(ARCHIVED_ERR),
-		modified: Joi.allow(null).error(MODIFIED_NOT_NULL_ERR)
+		created: Joi.date().timestamp().required().error('CREATED_DATE_ERR_SLOT'),
+		title: Joi.string().min(3).max(120).required().error('TITLE_ERR_SLOT'),
+		description: Joi.string().max(300).optional().error('DESC_ERR_SLOT'),
+		fallbackMediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error('IPFS_URL_ERR'),
+		fallbackMediaMime: Joi.string().valid(mimeTypes).required().error('MEDIA_MIME_ERR'),
+		fallbackTargetUrl: Joi.string().uri().required().error('TARGET_URL_ERR'),
+		archived: Joi.bool().optional().error('ARCHIVED_ERR'),
+		modified: Joi.allow(null).error('MODIFIED_NOT_NULL_ERR')
 	},
 	adSlotPut: {
-		title: Joi.string().max(120).required().error(TITLE_ERR_SLOT),
-		description: Joi.string().max(300).required().error(DESC_ERR_SLOT),
-		fallbackMediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error(IPFS_URL_ERR),
-		fallbackMediaMime: Joi.string().valid(mimeTypes).required().error(MEDIA_MIME_ERR),
-		fallbackTargetUrl: Joi.string().uri().required().error(TARGET_URL_ERR),
-		archived: Joi.bool().required().error(ARCHIVED_ERR),
-		modified: Joi.date().timestamp().required().error(MODIFIED_NOT_TIMESTAMP_ERR)
+		title: Joi.string().min(3).max(120).required().error('TITLE_ERR_SLOT'),
+		description: Joi.string().max(300).optional().error('DESC_ERR_SLOT'),
+		fallbackMediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error('IPFS_URL_ERR'),
+		fallbackMediaMime: Joi.string().valid(mimeTypes).required().error('MEDIA_MIME_ERR'),
+		fallbackTargetUrl: Joi.string().uri().required().error('TARGET_URL_ERR'),
+		archived: Joi.bool().required().error('ARCHIVED_ERR'),
+		modified: Joi.date().timestamp().required().error('MODIFIED_NOT_TIMESTAMP_ERR')
 	},
 	adUnitPost: {
-		type: Joi.string().regex(typeRegex).allow(types).required().error(TYPE_ERR_UNIT),
-		mediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error(IPFS_URL_ERR),
-		mediaMime: Joi.string().valid(mimeTypes).required().error(MEDIA_MIME_ERR),
-		targetUrl: Joi.string().uri().required().error(TARGET_URL_ERR),
+		type: Joi.string().regex(typeRegex).allow(types).required().error('TYPE_ERR_UNIT'),
+		mediaUrl: Joi.string().length(53).regex(ipfsRegex).required().error('IPFS_URL_ERR'),
+		mediaMime: Joi.string().valid(mimeTypes).required().error('MEDIA_MIME_ERR'),
+		targetUrl: Joi.string().uri().required().error('TARGET_URL_ERR'),
 		targeting: Joi.array().items({
-			tag: Joi.string().required().error(TAG_NAME_ERR),
-			score: Joi.number().min(0).max(100).required().error(TAG_SCORE_ERR)
+			tag: Joi.string().required().error('TAG_NAME_ERR'),
+			score: Joi.number().min(0).max(100).required().error('TAG_SCORE_ERR')
 		}).optional(),
 		tags: Joi.array().items({
-			tag: Joi.string().required().error(TAG_NAME_ERR),
-			score: Joi.number().min(0).max(100).required().error(TAG_SCORE_ERR)
+			tag: Joi.string().required().error('TAG_NAME_ERR'),
+			score: Joi.number().min(0).max(100).required().error('TAG_SCORE_ERR')
 		}).required(),
-		created: Joi.date().timestamp().required().error(CREATED_DATE_ERR_UNIT),
-		title: Joi.string().max(120).required().error(TITLE_ERR_UNIT),
-		description: Joi.string().max(300).required().error(DESC_ERR_UNIT),
-		archived: Joi.bool().optional().error(ARCHIVED_ERR),
-		modified: Joi.allow(null).error(MODIFIED_NOT_NULL_ERR)
+		created: Joi.date().timestamp().required().error('CREATED_DATE_ERR_UNIT'),
+		title: Joi.string().min(3).max(120).required().error('TITLE_ERR_UNIT'),
+		description: Joi.string().max(300).optional().error('DESC_ERR_UNIT'),
+		archived: Joi.bool().optional().error('ARCHIVED_ERR'),
+		modified: Joi.allow(null).error('MODIFIED_NOT_NULL_ERR')
 	},
 	adUnitPut: {
-		title: Joi.string().max(120).required().error(TITLE_ERR_UNIT),
-		description: Joi.string().max(300).required().error(DESC_ERR_UNIT),
-		archived: Joi.bool().required().error(ARCHIVED_ERR),
-		modified: Joi.date().timestamp().required().error(MODIFIED_NOT_TIMESTAMP_ERR)
+		title: Joi.string().min(3).max(120).required().error('TITLE_ERR_UNIT'),
+		description: Joi.string().max(300).optional().error('DESC_ERR_UNIT'),
+		archived: Joi.bool().required().error('ARCHIVED_ERR'),
+		modified: Joi.date().timestamp().required().error('MODIFIED_NOT_TIMESTAMP_ERR')
 	},
 	user: {
-		identity: Joi.string().regex(addressRegex).required().error(IDENTITY_ERR),
-		mode: Joi.number().valid(validModes).required().error(MODE_ERR),
-		signature: Joi.string().regex(signatureRegex).required().error(SIGNATURE_ERR),
-		hash: Joi.string().regex(hashRegex).required().error(HASH_ERR),
-		authToken: Joi.string().required().error(AUTH_TOKEN_ERR),
-		signerAddress: Joi.string().regex(addressRegex).required().error(SIGNER_ADDR_ERR),
-		prefixed: Joi.boolean().optional().error(PREFIXED_ERR),
+		identity: Joi.string().regex(addressRegex).required().error('IDENTITY_ERR'),
+		mode: Joi.number().valid(validModes).required().error('MODE_ERR'),
+		signature: Joi.string().regex(signatureRegex).required().error('SIGNATURE_ERR'),
+		hash: Joi.string().regex(hashRegex).required().error('HASH_ERR'),
+		authToken: Joi.string().required().error('AUTH_TOKEN_ERR'),
+		signerAddress: Joi.string().regex(addressRegex).required().error('SIGNER_ADDR_ERR'),
+		prefixed: Joi.boolean().optional().error('PREFIXED_ERR'),
 		typedData: Joi.array().items({
-			type: Joi.string().required().error(TD_TYPE_ERR),
-			name: Joi.string().required().error(TD_NAME_ERR),
-			value: Joi.string().required().error(TD_VALUE_ERR)
+			type: Joi.string().required().error('TD_TYPE_ERR'),
+			name: Joi.string().required().error('TD_NAME_ERR'),
+			value: Joi.string().required().error('TD_VALUE_ERR')
 		}).optional(),
-		role: Joi.string().valid(roles).optional().error(ROLE_ERR)
+		role: Joi.string().valid(roles).optional().error('ROLE_ERR')
 	}
 }

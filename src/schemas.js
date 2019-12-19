63e7b123808207aff3ffcb71c5c619a3ac3e11da
IvoPaunov <ivo.paunov@gmail.com>
2019-12-19 13:56:49 +0200

    fix campaign put scheme

--- src/schemas.js
+++ src/schemas.js
@@ -89,6 +89,6 @@ module.exports = {
         addr: Joi.string().regex(addressRegex).optional().error(new Error('VAL_ADDR_ERR'))
     },
     campaignPut: {
-        displayTitle: Joi.string().min(3).max(120).required().error(new Error('TITLE_ERR_CAMPAIGN')),
+        title: Joi.string().min(3).max(120).required().error(new Error('TITLE_ERR_CAMPAIGN')),
     }
 }

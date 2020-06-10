a45b9ad00e5ee1c612081424ee26d6bdad3deaf1
IvoPaunov <ivo.paunov@gmail.com>
2020-06-10 19:13:56 +0300

    init audienceInputPost schema

--- src/schemas.js
+++ src/schemas.js
@@ -98,5 +98,10 @@ module.exports = {
     },
     account: {
         email: Joi.string().email({ allowUnicode: false }).required().error(new Error(errors.ACCOUNT_EMAIL_ERR)),
+    },
+    audienceInputPost: {
+        version: Joi.string().min(1).max(69).required().error(new Error(errors.AUDIENCE_VERSION_ERR)),
+        inputs: Joi.object().required().error(new Error(errors.VAL_ADDR_ERR)),
     }
+
 }

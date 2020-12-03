f70666f9f312e7259cab1e4da211db65f60ea92b
IvoPaunov <ivo.paunov@gmail.com>
2020-12-03 16:41:26 +0200

    update campaign and audience schemas

--- src/schemas.js
+++ src/schemas.js
@@ -110,7 +110,8 @@ module.exports = {
             version: Joi.string().min(1).max(69).required().error(new Error(errors.AUDIENCE_VERSION_ERR)),
             inputs: Joi.object().required().error(new Error(errors.AUDIENCE_INPUTS_ERR)),
         }).allow(null).optional(),
-        pricingBoundsCPMUserInput: Joi.object().allow(null).optional()
+        pricingBoundsCPMUserInput: Joi.object().allow(null).optional(),
+        pricingBonds: Joi.object().allow(null).optional(),
     },
     account: {
         email: Joi.string().email({ allowUnicode: false }).required().error(new Error(errors.ACCOUNT_EMAIL_ERR)),
@@ -120,6 +121,7 @@ module.exports = {
         version: Joi.string().min(1).max(69).required().error(new Error(errors.AUDIENCE_VERSION_ERR)),
         inputs: Joi.object().required().error(new Error(errors.AUDIENCE_INPUTS_ERR)),
         title: Joi.string().allow(null).min(3).max(300).optional().error(new Error(errors.AUDIENCE_TITLE_ERR)),
+        pricingBoundsCPMUserInput: Joi.object().allow(null).optional()
     },
     audiencePut: {
         version: Joi.string().min(1).max(69).required().error(new Error(errors.AUDIENCE_VERSION_ERR)),
